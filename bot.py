be747ac7f137fc5ec034d29a1c91eb913879dd62
Abel325 <abelcheruiyot269@gmail.com>
2026-07-15 11:09:37 +0300

    Working Telegram bot

--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 
+BOT_TOKEN = "8870513554:AAEeAGTRpr9E9PvtW-znaRfhQJZmZser0Bo"
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "👋 Welcome to Official Waafi Loan Assistant!"
+    )
+
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("🤖 Waafi Loan Bot is running...")
+
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
